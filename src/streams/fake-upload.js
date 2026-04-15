320a2c8bc099b5b5135f9ac19027637ca3aee6fa
Victor Garcia <vfeitosa.garcia@gmail.com>
2026-04-15 15:58:05 -0300

    streams com http

---
+++ src/streams/fake-upload.js
@@ -0,0 +1,28 @@
+import { Readable } from "node:stream"
+
+// Readable Stream
+class OneToHundredStream extends Readable {
+  index = 1
+
+  // Método leitura do Readable
+  _read() {
+    const i = this.index++
+
+    setTimeout(() => {
+      if (i > 100) {
+        this.push(null) // "return null"
+      } else {
+        // "Buffer" -> Tipagem que node consegue entender
+        const buf = Buffer.from(String(i))
+
+        this.push(buf)
+      }
+    }, 500)
+  }
+}
+
+fetch("http://localhost:3434", {
+  method: "POST",
+  body: new OneToHundredStream(),
+  duplex: "half"
+})
