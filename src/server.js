92ae788d3c9a4f1f7f471effe45d6575d888f11a
Victor Garcia <vfeitosa.garcia@gmail.com>
2026-04-15 13:54:13 -0300

    salvando em memoria (stateful)

--- src/server.js
+++ src/server.js
@@ -1,12 +1,21 @@
 import http from "node:http"
 
+const users = []
+
 const server = http.createServer((req, res) => {
   const { method, url } = req
 
   if (url === "/users") {
     if (method === "GET") {
-      return res.end("Listagem de usuários")
+      return res
+        .setHeader("Content-type", "Application/json")
+        .end(JSON.stringify(users, null, 2))
     } else if (method === "POST") {
+      users.push({
+        id: 1,
+        name: "John Doe",
+        email: "johndoe@email.com"
+      })
       return res.end("Criação de usuários")
     }
   }
