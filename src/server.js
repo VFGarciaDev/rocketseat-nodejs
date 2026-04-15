6b4547a6fa93d2660f0392893894d9867764758f
Victor Garcia <vfeitosa.garcia@gmail.com>
2026-04-15 11:04:30 -0300

    rotas http PUT & GET

--- src/server.js
+++ src/server.js
@@ -1,8 +1,17 @@
 import http from "node:http"
 
 const server = http.createServer((req, res) => {
+  const { method, url } = req
+
+  if (url === "/users") {
+    if (method === "GET") {
+      return res.end("Listagem de usuários")
+    } else if (method === "POST") {
+      return res.end("Criação de usuários")
+    }
+  }
+
   return res.end("Hello World")
 })
 
 server.listen(3030)
-
